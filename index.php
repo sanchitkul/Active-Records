9565787038a5d4a95e9e4b4d7c9b06de55698faf
Your Name <you@example.com>
2017-11-18 12:14:56 -0500

    Displayed id=2 in array form

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <?php
+
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+
 define('DATABASE', 'ssk98');
 define('USERNAME', 'ssk98');
 define('PASSWORD', 'shaffer58');
-define('CONNECTION', 'sql1.njit.edu');
+define('CONNECTION', 'sql2.njit.edu');
 class dbConnection
 {
 protected static $connection;
@@ -32,15 +36,12 @@ class collection {
 protected $html;
 static public function newdb() 
 {
-
 $model = new static::$modelName;
 return $model;
-
 }
 static public function findAll()
 {
-
-$db = dbConn::getConnection();
+$db = dbConnection::getConnection();
 $tableName = get_called_class();
 $sql = 'SELECT * FROM ' . $tableName;
 $statement = $db->prepare($sql);
@@ -49,12 +50,10 @@ $class = static::$modelName;
 $statement->setFetchMode(PDO::FETCH_CLASS, $class);
 $recordsSet =  $statement->fetchAll();
 return $recordsSet;
-
 }
 static public function findOne($id) 
 {
-
-$db = dbConn::getConnection();
+$db = dbConnection::getConnection();
 $tableName = get_called_class();
 $sql = 'SELECT * FROM ' . $tableName . ' WHERE id =' . $id;
 $statement = $db->prepare($sql);
@@ -63,45 +62,26 @@ $class = static::$modelName;
 $statement->setFetchMode(PDO::FETCH_CLASS, $class);
 $recordsSet =  $statement->fetchAll();
 return $recordsSet;
-
 }
 public static function deleteRecord($id)
 {
-
-$db = dbConn::getConnection();
+$db = dbConnection::getConnection();
 $tableName = get_called_class();
 $sql = 'DELETE FROM '.$tableName.' WHERE id='.$id;
 echo $sql;
 $statement = $db->prepare($sql);
 $statement->execute();
 echo 'done';
-
 }
-
 }
 class accounts extends collection 
 {
-
 protected static $modelName = 'account';
-
 }
 class todos extends collection 
 {
-
 protected static $modelName = 'todo';
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
+$records = accounts::findOne(2);
+print_r($records);
 ?>
