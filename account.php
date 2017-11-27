55e8707210af9863b5568ec813e99cbff0beca1d
Sanchit Kulkarni <31752283+sanchitkul@users.noreply.github.com>
2017-11-26 20:39:12 -0500

    Final Commit

---
+++ account.php
@@ -0,0 +1,17 @@
+<?php
+
+ class account extends model 
+{
+public $id;
+public $email;
+public $fname;
+public $lname;
+public $phone;
+public $birthday;
+public $gender;
+public $password;
+public function __construct()
+{
+ $this->tableName = 'accounts';
+}
+}
